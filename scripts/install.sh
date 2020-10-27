995d8e8997fde0733f5e43ac4de2de4ffa729e63
Danny Korpan <8004507+dannykorpan@users.noreply.github.com>
2020-10-27 11:10:40 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# teamviewer controller
-CTL="${BASEURL}index.php?/module/teamviewer/"
+# AnyDesk controller
+CTL="${BASEURL}index.php?/module/anydesk/"
 
 # Get the scripts in the proper directories
-"${CURL[@]}" "${CTL}get_script/teamviewer" -o "${MUNKIPATH}preflight.d/teamviewer"
+"${CURL[@]}" "${CTL}get_script/anydesk" -o "${MUNKIPATH}preflight.d/anydesk"
 
 # Check exit status of curl
 if [ $? = 0 ]; then
 	# Make executable
-	chmod a+x "${MUNKIPATH}preflight.d/teamviewer"
+	chmod a+x "${MUNKIPATH}preflight.d/anydesk"
 
 	# Set preference to include this file in the preflight check
-	setreportpref "teamviewer" "${CACHEPATH}teamviewer.plist"
+	setreportpref "anydesk" "${CACHEPATH}anydesk.plist"
 
 else
 	echo "Failed to download all required components!"
-	rm -f "${MUNKIPATH}preflight.d/teamviewer"
+	rm -f "${MUNKIPATH}preflight.d/anydesk"
 
 	# Signal that we had an error
 	ERR=1
